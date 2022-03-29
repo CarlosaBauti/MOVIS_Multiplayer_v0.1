58d83815592a6dc92731575703c3832ea6bc8594
Carlos Bautista <CarlosaBauti@users.noreply.github.com>
2022-03-28 22:31:02 -0400

    Implementation of JoinRoom_Panel
    Creates the UserTypeManager class to assign user types and creates a JoinRoom button to join rooms

---
+++ Assets/Scripts/UserTypeManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserTypeManager : MonoBehaviour
+{
+    public static bool userType; //true is tablet user and false is HMD user
+
+    public void ClickedHeadset()
+    {
+        userType = false; //set as HMD user
+    }
+    
+    public void ClickedTablet()
+    {
+        userType = true; // set as Tablet user
+    }
+}
